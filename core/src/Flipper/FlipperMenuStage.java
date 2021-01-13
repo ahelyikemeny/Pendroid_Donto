b6415999dfd5580608825da4b5f5c66a7f09f0b1
Milaan1337 <71323999+Milaan1337@users.noreply.github.com>
2021-01-13 10:43:40 +0100

    onclick

--- core/src/Flipper/FlipperMenuStage.java
+++ core/src/Flipper/FlipperMenuStage.java
@@ -10,5 +10,8 @@ import hu.csanyzeg.master.MyBaseClasses.Scene2D.ResponseViewport;
 public class FlipperMenuStage extends MyStage {
     public FlipperMenuStage(MyGame game) {
         super(new ExtendViewport(160,90), game);
+        addActor(new FlipperMenuBackgroundActor(game, 160,120,0,0) );
+        addActor(new FlipperStartButton(game, 40, 10, 0, 80 ));
+
     }
 }
