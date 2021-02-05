4d6b1c45787eb9657e90c11473574dd87186754f
Mate Kancsal <minecraft.bencmet@gmail.com>
2021-02-05 08:17:13 +0100

    Projekt fix

--- core/src/Flipper/BgActor.java
+++
@@ -1,9 +0,0 @@
-package Flipper;
-
-import hu.csanyzeg.master.MyBaseClasses.Box2dWorld.Box2dStage;
-
-public class BgActor extends BaseHitboxActor {
-    public BgActor(Box2dStage stage) {
-        super(stage, "Flipper/map.png", "map", 160, 160);
-    }
-}
